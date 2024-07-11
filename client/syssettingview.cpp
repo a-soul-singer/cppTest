d792943e85ec2e17a3008c25acc211c155bc4ebc
Qiaochenhuan <609989627@qq.com>
2024-07-11 23:21:19 +0800

    主题设置

--- client/syssettingview.cpp
+++ client/syssettingview.cpp
@@ -8,16 +8,21 @@ SysSettingView::SysSettingView(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QStringList crr={"黑色","白色","灰色"};
+    QStringList crr={"蓝色妖姬","红色风暴","绿色毒刺","粉色回忆","黑色沉默","灰色轨迹","银色杀手","金色传说"};
     ui->comboBoxSysSetting->addItems(crr);
     ui->comboBoxSysSetting->setCurrentIndex(-1);
     ui->spinBox->setRange(10, 50);
     ui->spinBox->setValue(20);
 
     m_map = {
-        {"黑色","../client/qss/SysSettingBlack.qss"},
-        {"白色","../client/qss/SysSettingWhite.qss"},
-        {"灰色","../client/qss/SysSettingGray.qss"},
+        {"蓝色妖姬","../client/qss/SysSettingBlue.qss"},
+        {"红色风暴","../client/qss/SysSettingRed.qss"},
+        {"绿色毒刺","../client/qss/SysSettingGreen.qss"},
+        {"粉色回忆","../client/qss/SysSettingPink.qss"},
+        {"黑色沉默","../client/qss/SysSettingBlack.qss"},
+        {"灰色轨迹","../client/qss/SysSettingGray.qss"},
+        {"银色杀手","../client/qss/SysSettingSilver.qss"},
+        {"金色传说","../client/qss/SysSettingGilden.qss"},
     };
 
     connect(ui->comboBoxSysSetting, &QComboBox::currentTextChanged, this, &SysSettingView::setStylesheetFromFile);
@@ -25,6 +30,32 @@ SysSettingView::SysSettingView(QWidget *parent) :
     connect(ui->spinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, &SysSettingView::setFontSize);
     // 从数据库中读取当前的主题的设置
     //    setStylesheetFromFile("灰色", false);
+
+
+//    m_sqlcls = SqliteClient::getInstance();
+//    QString sql="insert into t_style_setting(styleName) values('%1');";
+//    if(!m_sqlcls->execOtherSql(sql.arg("黑色"))){
+//        return;
+//    }
+//    sql="select styleName from t_style_setting";
+//    auto res=m_sqlcls->execQuerySqlLimitOne(sql);
+//    if(!res.isEmpty()){
+//        ui->comboBoxSysSetting->setStyleSheet(res.front().toString());
+//    }
+
+    m_sqlcls = SqliteClient::getInstance();
+    QString sql="insert into t_style_setting(styleName) values('%1');";
+
+//    foreach (const QString &key, m_map.keys()) {
+//        QString sql="insert into t_style_setting(styleName) values('%1');";
+//        m_sqlcls->execOtherSql(sql.arg(ui->comboBoxSysSetting->currentText()));
+//    }
+
+    sql="select * from t_style_setting";
+    auto res=m_sqlcls->execQuerySqlLimitOne(sql);
+    if(!res.isEmpty()){
+        ui->comboBoxSysSetting->setStyleSheet(res.front().toString());
+    }
 }
 
 SysSettingView::~SysSettingView()
@@ -51,3 +82,15 @@ void SysSettingView::setFontSize(int fontSize)
 {
     emit setStyleSheetSignal(QString("*{font-size:%1px;}").arg(fontSize));
 }
+
+void SysSettingView::on_comboBoxSysSetting_currentTextChanged(const QString &arg1)
+{
+    m_sqlcls = SqliteClient::getInstance();
+    QString sql="TRUNCATE TABLE t_style_setting;";
+    m_sqlcls->execOtherSql(sql);
+    foreach (const QString &key, m_map.keys()) {
+        QString sql="insert into t_style_setting(styleName) values('%1');";
+        m_sqlcls->execOtherSql(sql.arg(ui->comboBoxSysSetting->currentText()));
+    }
+}
+
